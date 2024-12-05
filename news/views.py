d6660ecc988f9bf82dc44642b34cc09672c549b8
arnaudstdr <arnaud.stadler@ikmail.com>
2024-12-05 12:51:05 +0100

    meilleure pertinence des résultats

--- news/views.py
+++ news/views.py
@@ -6,7 +6,7 @@ from django.conf import settings
 # Create your views here.
 def search_news_view(request):
     api_key = settings.NEWS_API_KEY
-    query = 'machine learning OR deep learning OR intelligence artificielle OR cybersécurité'
+    query = '"machine learning" OR "deep learning" OR "intelligence artificielle" OR "cybersécurité" OR "programmation" OR "open source"'
     to_date = datetime.today().strftime('%Y-%m-%d')
     from_date = (datetime.today() - timedelta(days=7)).strftime('%Y-%m-%d')
 
@@ -16,7 +16,7 @@ def search_news_view(request):
         'from': from_date,
         'to': to_date,
         'language': 'fr',
-        'sortBy': 'publishedAt',
+        'sortBy': 'relevance',
         'apiKey': api_key,
         'pageSize': '50'
     }
@@ -24,4 +24,10 @@ def search_news_view(request):
     response = requests.get(url, params=params)
     articles = response.json().get('articles', [] if response.status_code == 200 else [])
 
+    # Rue par date côté serveur si la date publication existe
+    articles = sorted(
+        articles,
+        key=lambda article: article.get('publishedAt'),
+        reverse=True # Du plus récent au plus ancien
+    )
     return render(request, 'news/search_news.html', {'articles': articles})
